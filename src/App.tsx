c0aca2acfa5c8ee9419af2f4209cf3597dc74b63
john-seredich <seredichjohnalt@gmail.com>
2022-12-20 17:20:38 -0500

    feat: setup and link RouteProvider and QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  const queryClient = new QueryClient();
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
