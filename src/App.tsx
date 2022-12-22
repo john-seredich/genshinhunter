8c3dbee61c630aa38260b382ffcc1be8d6b3d8b7
john-seredich <seredichjohnalt@gmail.com>
2022-12-22 14:30:14 -0500

    feat: create weapons page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./routes/Homepage/Homepage";
+import Weapons from "./routes/Weapons/Weapons";
 
 function App() {
   const queryClient = new QueryClient();
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Homepage />,
+    },
+    {
+      path: "/weapons",
+      element: <Weapons />,
     },
   ]);
 
