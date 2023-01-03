4b448a1d4ef6a88482f8a5bdba7a8ed36a9b3aea
john-seredich <seredichjohnalt@gmail.com>
2023-01-03 12:55:47 -0500

    feat: header link icons and styles changes

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { links } from "../../links";
 import styles from "./Header.module.scss";
+import img from "../../assets/images/Icon_Inventory_Weapons.webp";
 
 function Header() {
   const linkElement = links.map((link) => {
     return (
-      <li key={link}>
-        <Link to={link.toLowerCase()}>{link}</Link>
+      <li key={link.id}>
+        <Link to={link.name.toLowerCase()}>
+          <img src={link.img} alt="" />
+        </Link>
       </li>
     );
   });
@@ -15,7 +18,7 @@ function Header() {
     <header className={styles.header}>
       <h1>GenshinHunter</h1>
       <nav>
-        <ul className={styles.nav}>{linkElement}</ul>
+        <ul className={styles.header__ul}>{linkElement}</ul>
       </nav>
     </header>
   );
