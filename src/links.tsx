dc248e45022625116b4d40c486c49c4a1d24dcd9
john-seredich <seredichjohnalt@gmail.com>
2023-01-04 14:19:55 -0500

    feat: new route value in links object

--- src/links.tsx
+++ src/links.tsx
@@ -11,46 +11,55 @@ import furnshingImg from "./assets/images/inventory_icon_furnishings.webp";
 export const links = [
   {
     name: "Weapons",
+    route: "weapons",
     img: weaponImg,
     id: 1,
   },
   {
     name: "Artifacts",
+    route: "artifacts",
     img: artifactImg,
     id: 2,
   },
   {
     name: "Character Development Items",
+    route: "devitems",
     img: characterDevImg,
     id: 3,
   },
   {
     name: "Food",
+    route: "food",
     img: foodImg,
     id: 4,
   },
   {
     name: "Materials",
+    route: "materials",
     img: materialImg,
     id: 5,
   },
   {
     name: "Gadgets",
+    route: "gadgets",
     img: gadgetImg,
     id: 6,
   },
   {
     name: "Quests",
+    route: "quests",
     img: questImg,
     id: 7,
   },
   {
     name: "Precious Items",
+    route: "precious",
     img: preciousImg,
     id: 8,
   },
   {
     name: "Furnishings",
+    route: "furnishings",
     img: furnshingImg,
     id: 9,
   },
