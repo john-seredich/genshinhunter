1a7b2b54e75a68c6bc96f1da86cb13123c547403
john-seredich <seredichjohnalt@gmail.com>
2023-01-04 18:33:08 -0500

    feat: menu toggle state and handler

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { links } from "../../links";
+import { useState } from "react";
 import styles from "./Header.module.scss";
 import { FaBars } from "react-icons/fa";
 
 function Header() {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const toggleMenuHandler = () => setMenuToggle((prevToggle) => !prevToggle);
+
   const linkElement = links.map((link) => {
     return (
       <li key={link.id}>
@@ -14,20 +18,19 @@ function Header() {
     );
   });
 
-  const mobileMenu = () => {
-    return <ul>{linkElement}</ul>;
-  };
-
   return (
-    <header className={styles.header}>
-      <h1>GenshinHunter</h1>
-      <nav>
-        <ul className={styles.header__ul}>{linkElement}</ul>
-        <button className={styles.header__btn}>
-          <FaBars />
-        </button>
-      </nav>
-    </header>
+    <>
+      <header className={styles.header}>
+        <h1>GenshinHunter</h1>
+        <nav>
+          <ul className={styles.header__ul}>{linkElement}</ul>
+          <button className={styles.header__btn} onClick={toggleMenuHandler}>
+            <FaBars />
+          </button>
+        </nav>
+      </header>
+      {menuToggle && <p>Menu Toggled</p>}
+    </>
   );
 }
 
