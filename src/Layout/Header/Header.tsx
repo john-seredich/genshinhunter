71a1a2f8eff0b81465f969f1f769c191b29b3220
john-seredich <seredichjohnalt@gmail.com>
2023-01-04 13:16:49 -0500

    feat: fix links appending url, media queries for header nav

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,24 +1,31 @@
 import { Link } from "react-router-dom";
 import { links } from "../../links";
 import styles from "./Header.module.scss";
-import img from "../../assets/images/Icon_Inventory_Weapons.webp";
+import { FaBars } from "react-icons/fa";
 
 function Header() {
   const linkElement = links.map((link) => {
     return (
       <li key={link.id}>
-        <Link to={link.name.toLowerCase()}>
+        <Link to={`/${link.name.toLowerCase()}`}>
           <img src={link.img} alt="" />
         </Link>
       </li>
     );
   });
 
+  const mobileMenu = () => {
+    return <ul>{linkElement}</ul>;
+  };
+
   return (
     <header className={styles.header}>
       <h1>GenshinHunter</h1>
       <nav>
         <ul className={styles.header__ul}>{linkElement}</ul>
+        <button className={styles.header__btn}>
+          <FaBars />
+        </button>
       </nav>
     </header>
   );
