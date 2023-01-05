98a52242d9592eac4bc4ded297d666a19623ff2b
john-seredich <seredichjohnalt@gmail.com>
2023-01-05 16:15:23 -0500

    feat: active styles for links, auto-close menu, and mobile menu styles

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,22 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { links } from "../../links";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Header.module.scss";
 import { FaBars } from "react-icons/fa";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 function Header() {
+  const location = useLocation();
+  const currentPath = location.pathname.slice(1);
+  const { width } = useWindowDimensions();
   const [menuToggle, setMenuToggle] = useState(false);
   const toggleMenuHandler = () => setMenuToggle((prevToggle) => !prevToggle);
 
+  // Mobile Menu Disabled when exceeds 765px
+  useEffect(() => {
+    if (width > 1000) setMenuToggle(false);
+  }, [width, setMenuToggle]);
+
+  // Dynamic Link element to detect current active page.
   const linkElement = links.map((link) => {
+    let currentImage: any;
+    if (currentPath === link.route) {
+      currentImage = (
+        <img key={link.id} className={styles.active} src={link.img} alt="" />
+      );
+    } else {
+      currentImage = <img key={link.id} src={link.img} alt="" />;
+    }
+
     return (
       <li key={link.id}>
-        <Link to={`/${link.route}`}>
-          <img src={link.img} alt="" />
-        </Link>
+        <Link to={`/${link.route}`}>{currentImage}</Link>
       </li>
     );
   });
+
+  // Mobile Menu Element
+  const MobileMenu = () => {
+    return (
+      <nav className={styles.mobile_menu}>
+        <ul>{linkElement}</ul>
+      </nav>
+    );
+  };
 
   return (
     <>
@@ -29,7 +55,7 @@ function Header() {
           </button>
         </nav>
       </header>
-      {menuToggle && <p>Menu Toggled</p>}
+      {menuToggle && <MobileMenu />}
     </>
   );
 }
