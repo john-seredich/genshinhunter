92aca003cd462520e3448f2ab8ea436200ea8798
john-seredich <seredichjohnalt@gmail.com>
2023-01-10 14:22:37 -0500

    feat: blank stats card styles

--- src/components/StatsCard/StatsCard.tsx
+++ src/components/StatsCard/StatsCard.tsx
@@ -1,5 +1,7 @@
+import styles from "./StatsCard.module.scss";
+
 function StatsCard() {
-  return <div>StatsCard</div>;
+  return <div className={styles.stats_card}>StatsCard</div>;
 }
 
 export default StatsCard;
