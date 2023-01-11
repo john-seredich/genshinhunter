8623e6a792e68e45a1160cf58327d333516d349f
john-seredich <seredichjohnalt@gmail.com>
2023-01-11 14:16:03 -0500

    feat: incomplete footer component

---
+++ src/Layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+export default Footer;
