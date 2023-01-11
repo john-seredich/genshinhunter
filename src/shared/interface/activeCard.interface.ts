e4f00cdeb1f5251d51eefeb2bf22022251637606
john-seredich <seredichjohnalt@gmail.com>
2023-01-11 16:13:41 -0500

    feat: interface containing activeCard data

---
+++ src/shared/interface/activeCard.interface.ts
@@ -0,0 +1,15 @@
+// export interface IActiveCard {
+//   name: string;
+//   id: string;
+// }
+
+export interface IActiveCard {
+  name: string;
+  rarity: number;
+  type: string;
+  baseAttack: number;
+  subStat: string;
+  passiveName: string;
+  passiveDesc: string;
+  id: string;
+}
