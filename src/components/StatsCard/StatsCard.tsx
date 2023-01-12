60b4f840b10a5ba2c217d81ca8de827a10ce2ff2
john-seredich <seredichjohnalt@gmail.com>
2023-01-12 14:26:10 -0500

    fix: no text for rarity 1 item passive & description

--- src/components/StatsCard/StatsCard.tsx
+++ src/components/StatsCard/StatsCard.tsx
@@ -2,6 +2,22 @@ import { IActiveCard } from "../../shared/interface/activeCard.interface";
 import styles from "./StatsCard.module.scss";
 
 function StatsCard(props: IActiveCard) {
+  const description = () => {
+    return (
+      <>
+        <p>
+          {props.passiveName.length === 1 ? "No Passive" : props.passiveName}
+        </p>
+        <p>
+          -{" "}
+          {props.passiveDesc.length === 1
+            ? "No Description"
+            : props.passiveName}
+        </p>
+      </>
+    );
+  };
+
   return (
     <div className={`${styles.stats_card} ${styles[`rarity-${props.rarity}`]}`}>
       <h2 className={styles.stats_card__name}>{props.name}</h2>
@@ -21,8 +37,14 @@ function StatsCard(props: IActiveCard) {
         />
       </div>
       <div className={styles.stats_card__description}>
-        <p>{props.passiveName}</p>
-        <p>- {props.passiveDesc}</p>
+        <p>
+          {props.passiveName.length === 1 ? "No Passive" : props.passiveName}
+        </p>
+        <p>
+          {props.passiveDesc.length === 1
+            ? "No Description"
+            : "- " + props.passiveDesc}
+        </p>
       </div>
     </div>
   );
