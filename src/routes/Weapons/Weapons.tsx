a3ab5ca9ccdc036223e9b46b3926be03a88402c7
john-seredich <seredichjohnalt@gmail.com>
2023-01-19 16:17:29 -0500

    feat: feat: fetch now both weapons name and data together

--- src/routes/Weapons/Weapons.tsx
+++ src/routes/Weapons/Weapons.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import StatsCard from "../../components/StatsCard/StatsCard";
-import TestCard from "../../components/WeaponCard/TestCard";
 import WeaponCard from "../../components/WeaponCard/WeaponCard";
 import Footer from "../../Layout/Footer/Footer";
 import Header from "../../Layout/Header/Header";
@@ -10,13 +9,12 @@ import { IActiveCard } from "../../shared/interface/activeCard.interface";
 import { staticItemData } from "../../staticItemData";
 import styles from "./Weapons.module.scss";
 
-const fetchWeaponsList = () => axios.get("https://api.genshin.dev/weapons");
-
 const fetchWeapons = async () => {
   const weaponsList = await axios.get("https://api.genshin.dev/weapons");
   const weapons = await Promise.all(
-    weaponsList?.data.map((weapon: string) => {
-      return axios.get(`https://api.genshin.dev/weapons/${weapon}`);
+    weaponsList?.data.map(async (weapon: string) => {
+      const res = await axios.get(`https://api.genshin.dev/weapons/${weapon}`);
+      return { name: weapon, data: res.data };
     })
   );
   return weapons;
@@ -25,29 +23,7 @@ const fetchWeapons = async () => {
 function Weapons() {
   const [activeCard, setActiveCard] = useState<IActiveCard>(staticItemData);
   const [cardToggle, setCardToggle] = useState(true);
-  const { data: weaponList } = useQuery(["weaponList"], fetchWeaponsList);
   const { data } = useQuery(["weaponsTest"], fetchWeapons);
-
-  // const weaponCardElement = () => {
-  //   const weaponData = data
-  //     ?.map((obj) => {
-  //       return obj.data;
-  //     })
-  //     .map((weapon) => {
-  //       return (
-  //         <WeaponCard
-  //           key={weapon.name}
-  //           name={weapon}
-  //           setActiveCard={setActiveCard}
-  //           activeCardName={activeCard.id}
-  //           setCardToggle={setCardToggle}
-  //         />
-  //       );
-  //     });
-  //   return weaponData;
-  // };
-
-  console.log(data?.[0].data);
 
   return (
     <>
@@ -56,23 +32,19 @@ function Weapons() {
         <div className={styles.container}>
           {data
             ?.map((obj) => {
-              return obj.data;
+              return obj;
             })
-            .map((weapon, i: number) => {
-              return <TestCard key={i} weapon={weapon} />;
+            .map((weapon) => {
+              return (
+                <WeaponCard
+                  key={weapon.name}
+                  weapon={weapon}
+                  setActiveCard={setActiveCard}
+                  activeCardName={activeCard.id}
+                  setCardToggle={setCardToggle}
+                />
+              );
             })}
-
-          {/* {weaponList?.data.map((item: string) => {
-            return (
-              <WeaponCard
-                key={item}
-                name={item}
-                setActiveCard={setActiveCard}
-                activeCardName={activeCard.id}
-                setCardToggle={setCardToggle}
-              />
-            );
-          })} */}
         </div>
         {cardToggle && (
           <StatsCard activeCard={activeCard} setCardToggle={setCardToggle} />
