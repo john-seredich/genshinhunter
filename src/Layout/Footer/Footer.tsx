b9c5dc2c929f2561c6b3b485a2b501880d0d209c
john-seredich <seredichjohnalt@gmail.com>
2023-01-24 18:26:14 -0500

    feat: clickHandler to sort items by rarity

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,11 +1,19 @@
 import styles from "./Footer.module.scss";
 import { HiArrowsUpDown } from "react-icons/hi2";
 
-function Footer() {
+function Footer(props: any) {
+  const clickHandler = () => {
+    if (props.sort === "asc") {
+      props.setSort("desc");
+    } else {
+      props.setSort("asc");
+    }
+  };
+
   return (
     <footer className={styles.footer}>
       <button className={styles.footer__dropbtn}>Quality</button>
-      <button className={styles.footer__orderbtn}>
+      <button className={styles.footer__orderbtn} onClick={clickHandler}>
         <HiArrowsUpDown />
       </button>
     </footer>
