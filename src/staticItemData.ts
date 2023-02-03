7c9b1e42bc9dbaa5a07ddd85f946d5ccf3e6a09f
john-seredich <seredichjohnalt@gmail.com>
2023-02-03 15:11:52 -0500

    feat: static food data for Food component

--- src/staticItemData.ts
+++ src/staticItemData.ts
@@ -9,3 +9,12 @@ export const staticItemData = {
     "For every point of the entire party's combined maximum Energy capacity, the Elemental Burst DMG of the character equipping this weapon is increased by 0.12/0.15/0.18/0.21/0.24%. A maximum of 40/50/60/70/80% increased Elemental Burst DMG can be achieved this way.",
   id: "akuoumaru",
 };
+
+export const staticFoodData = {
+  name: "Adventurer's Breakfast Sandwich",
+  rarity: 3,
+  effect: "ATK-Boosting Dish",
+  hasRecipe: "Increase all party members' ATK by 160~228 for 300s",
+  description: "A nutritious egg dish. L…s top-rated breakfasts.",
+  proficiency: true,
+};
